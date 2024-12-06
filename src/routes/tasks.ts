e595f1939e0f4033cd10b8e27c3e7dd1974d3882
Unicornelia <ez.kornelia@gmail.com>
2024-12-06 13:04:29 +0100

    feat: add post route for creating a task

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,7 +1,19 @@
-import {Router} from "express";
-import {Task} from "../models/task.js";
+import { Router, Request, Response } from "express";
+import { Task } from "../models/task.js";
 
 const router = Router();
 let tasks: Task[] = [];
 
-export default router
+router.post("/", async (req: Request, res: Response) => {
+  const task: Task = {
+    id: tasks.length + 1,
+    title: req.body.title,
+    description: req.body.description,
+    completed: false,
+  };
+
+  tasks.push(task);
+  res.status(201).json(task);
+});
+
+export default router;
