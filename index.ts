0834d3a33b87b14e53f23794f4e7e02d266ac41e
Unicornelia <ez.kornelia@gmail.com>
2024-12-06 13:04:04 +0100

    fix: prettier

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
-import express, {Request, Response} from 'express'; //Import the express dependency
+import express, { Request, Response } from "express"; //Import the express dependency
 
-const app = express();             //Instantiate an express app, the main work horse of this server
-const port = process.env.PORT || 3000;                  //Save the port number where your server will be listening
+const app = express(); //Instantiate an express app, the main work horse of this server
+const port = process.env.PORT || 3000; //Save the port number where your server will be listening
 
 //Idiomatic expression in express to route and respond to a client request
-app.get('/', (req: Request, res: Response) => { //get requests to the root ("/") will route here
-    res.send('Hello World!');
-    // res.sendFile('src/index.ts', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
+app.get("/", (req: Request, res: Response) => {
+  //get requests to the root ("/") will route here
+  res.send("Hello World!");
+  // res.sendFile('src/index.ts', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+  //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
 });
 
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`);
+app.listen(port, () => {
+  //server starts listening for any attempts from a client to connect at port: {port}
+  console.log(`Now listening on port ${port}`);
 });
